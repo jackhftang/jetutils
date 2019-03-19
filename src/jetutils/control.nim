7b49cd56928e1251fcc79436755d15bdf8231d17
Jack Tang <me@jackhftang.com>
2019-03-19 23:21:33 +0800

    ADD: added forMin, forSum, forProd

--- src/jetutils/control.nim
+++ src/jetutils/control.nim
@@ -1,28 +1,40 @@
-proc bsearchMax*[T:Ordinal](a, b: T, prediate: proc (mi: T): bool  {.closure.}): T = 
+import macros
+
+template bsearchMaxIt*(a, b: typed, code: untyped): untyped = 
+  type outType = type(a)
+  var
+    lo = ord(a)
+    up = ord(b)
+  while lo + 1 < up : 
+    let mi = (lo + up) shr 1
+    let it {.inject.} = outType(mi)
+    if code : lo = mi
+    else: up = mi
+  outType(lo)
+
+proc bsearchMax*[T:Ordinal](a, b: T, predicate: proc (mi: T): bool  {.closure.}): T = 
   ## Find the maximum value mx between [a,b) such that `predicate(mx)` is true
   ##
   ## NOTE: It is assumed that `predicate(a)` is always true. In case all values in [a,b) yield false, a is returned.
+  bsearchMaxIt(a,b,predicate it)
+
+template bsearchMinIt*(a, b: typed, code: untyped): untyped = 
+  type outType = type(a)
   var
     lo = ord(a)
     up = ord(b)
   while lo + 1 < up : 
-    let mi = (lo + up) shr 1 
-    if prediate mi.T: lo = mi 
-    else: up = mi
-  return lo.T
+    let mi = (lo + up) shr 1
+    let it {.inject.} = outType(mi)
+    if code : up = mi
+    else: lo = mi
+  outType(up)
 
-proc bsearchMin*[T:Ordinal](a, b: T, prediate: proc (mi: T): bool  {.closure.}): T = 
+proc bsearchMin*[T:Ordinal](a, b: T, predicate: proc (mi: T): bool  {.closure.}): T = 
   ## Find the minimum value mi between (a,b] such that `predicate(mi)` is true
   ##
   ## NOTE: It is assumed that `predicate(b)` is always true. In case all values in (a,b] yield false, b is returned.
-  var
-    lo = ord(a)
-    up = ord(b)
-  while lo + 1 < up : 
-    let mi = (lo + up) shr 1 
-    if prediate mi.T: up = mi 
-    else: lo = mi
-  return up.T
+  bsearchMinIt(a,b,predicate it)
 
 template times*(n:int, body:untyped) = 
   ## Repeat block of code N times.
@@ -33,3 +45,215 @@ template times*(n:int, body:untyped) =
   ##    echo "ho"
   for i in 1..n:
     body
+
+iterator `..<`*[T](b: T): T =
+  ## only high part need to be specified 
+  ## 
+  ## .. code-block:: nim 
+  ##  for i in ..< arr.len: 
+  ##    echo arr[i]
+  var i = T(0)
+  while i < b:
+    yield i
+    inc i
+
+
+macro forSum*(args: varargs[untyped]): untyped = 
+  ## transform to a serial of  `for` statement 
+  ##
+  ## ..code-block: nim
+  ##  forSum i in 1..10, "abcd": 
+  ##    echo i 
+  ## 
+  ## equivalent to 
+  ## 
+  ## ..code-block: nim 
+  ##  for i in 1..10:
+  ##    echo i 
+  ##  for i in "abcd":
+  ##    echo i
+
+  # deal with the in operator 
+  assert args[0].kind == nnkInfix
+  assert args[0].len == 3 
+  assert args[0][0].strVal == "in"
+  assert args[0][1].kind == nnkIdent, "the first argument must be identifier"
+
+  let ident = args[0][1] 
+  var iterators = @[args[0][2]]
+
+  # get iterators
+  for i in 1 .. args.len-2:
+    iterators.add(args[i])
+  
+  # code 
+  let code = args[args.len-1]
+
+  # generate code 
+  result = newStmtList()
+  for it in iterators:
+    var t = newNimNode(nnkForStmt)
+    t.add(ident)
+    t.add(it)
+    t.add(copyNimTree(code))
+    result.add(t)
+
+macro forProd*(args: varargs[untyped]): untyped = 
+  ## Transform to nested `for` statement 
+  ## 
+  ## Example:
+  ## 
+  ## ..code-block: nim
+  ##  forProd i,j,k in 1..10, "abc", [false, true]:
+  ##    echo i, " ", j, " ", k 
+  ## 
+  ## transform to
+  ## 
+  ## ..code-block: nim 
+  ##  for i in 1..10:
+  ##    for j in "abc":
+  ##      for k in [false, true]:
+  ##        echo i, " ", j, " ", k 
+  ##
+
+  var i = 0
+  var idents : seq[NimNode] = @[]
+    
+  # number of argument must be odd number 
+  assert args.len mod 2 == 0, "number of identifiers must equal to number of iterators"
+  var n = args.len div 2 
+
+  # get identifiers 
+  while i < args.len: 
+    # echo args[i].kind
+    let node = args[i]
+    if node.kind == nnkIdent:
+      idents.add(node)
+      i.inc
+    else:
+      break 
+  assert idents.len == n-1, "too many identifiers"
+
+  # deal with the in operator 
+  assert args[i].kind == nnkInfix
+  assert args[i].len == 3 
+  assert args[i][0].strVal == "in"
+  assert args[i][1].kind == nnkIdent
+  idents.add(args[i][1])
+
+  # get iterators
+  var iterators = @[args[i][2]]
+  i.inc 
+  while i < args.len - 1:
+    iterators.add(args[i])
+    i.inc 
+  
+  # code 
+  let code = args[i]
+
+  # generate code 
+  var forStmt = newNimNode(nnkForStmt)
+  forStmt.add(idents[n-1])
+  forStmt.add(iterators[n-1])
+  forStmt.add(code)
+  for i in countdown(n-2,0):
+    let t = newNimNode(nnkForStmt)
+    t.add(idents[i])
+    t.add(iterators[i])
+    t.add(forStmt)
+    forStmt = t 
+    
+  result = forStmt
+
+macro forMin*(args: varargs[untyped]): untyped = 
+  ## Each iterators need to have len() implementation
+  ## 
+  ## Example:
+  ## 
+  ## ..code-block: nim
+  ##  forMin i,j in [1,2,3], "abc":
+  ##    echo i, j
+  ## 
+  ## transform to
+  ## 
+  ## ..code-block: nim 
+  ##  block: 
+  ##    let sym1 = [1,2,3]
+  ##    let sym2 = "abc"
+  ##    let sym3 = min(ident1.len,ident2.len)
+  ##    for sym4 in 0 ..< sym3:
+  ##      let i = sym1[ident4]
+  ##      let j = sym2[ident4]
+  ##      echo i, j
+  ##      
+  ## symbols (sym1,sym2,sym3,sym4) are uniquely generated
+  ##
+
+  var i = 0
+  var idents : seq[NimNode] = @[]
+    
+  # number of argument must be odd number 
+  assert args.len mod 2 == 0, "number of identifiers must equal to number of lists"
+  var n = args.len div 2 
+
+  # get identifiers 
+  while i < args.len: 
+    # echo args[i].kind
+    let node = args[i]
+    if node.kind == nnkIdent:
+      idents.add(node)
+      i.inc
+    else:
+      break 
+  assert idents.len == n-1, "too many identifiers"
+
+  # deal with the in operator 
+  assert args[i].kind == nnkInfix
+  assert args[i].len == 3 
+  assert args[i][0].strVal == "in"
+  assert args[i][1].kind == nnkIdent
+  idents.add(args[i][1])
+
+  # get iterators
+  var lists = @[args[i][2]]
+  i.inc 
+  while i < args.len - 1:
+    lists.add(args[i])
+    i.inc 
+  
+  # code 
+  let code = args[i]
+
+  # generate code 
+  let stmts = newStmtList()
+
+  # let statements 
+  var symLists: seq[NimNode] = @[]
+  for it in lists:
+    let sym = genSym()
+    symLists.add sym
+    stmts.add newLetStmt(sym, it)
+
+  # minimum length among lists
+  let symMin = genSym()
+  var minExpr = newCall(ident("len"), symLists[0])
+  for i in 1..<symLists.len:
+    minExpr = newCall(ident("min"), newCall(ident("len"), symLists[i]), minExpr)
+  stmts.add newLetStmt(symMin, minExpr)
+
+  # for loop 
+  var symIx = genSym(nskForVar)
+  var forStmt = newNimNode(nnkForStmt)
+    .add(symIx)
+    .add(newNimNode(nnkInfix).add(ident "..<").add(newIntLitNode(0), symMin))
+
+  # for loop body
+  let body = newStmtList()
+  for i, ident in idents: 
+    body.add newLetStmt(ident, newNimNode(nnkBracketExpr).add(symLists[i]).add(symIx))
+  body.add code
+  forStmt.add body
+
+  stmts.add forStmt
+  result = newBlockStmt(stmts)
+  
--- tests/test.nim
+++ tests/test.nim
@@ -2,93 +2,149 @@ import unittest
 import jetutils
 import tables
 
-test "bsearchMax":
-  let res = bsearchMax(0, 10) do (n:int) -> bool: n*n < 10
-  check res == 3
+suite "control": 
+  type 
+    Foo = enum 
+      fooA, fooB, fooC, fooD, fooE, fooF, fooG
+  
+  test "bsearchMaxIt (enum)":
+    check bsearchMaxIt(fooA, fooG, it <= fooE) == fooE
 
-test "bsearchMin":
-  let res = bsearchMin('a', 'z', proc (c:char): bool = c >= 'c')
-  check res == 'c'
+  test "bsearchMaxIt":
+    check bsearchMaxIt(0, 10, it <= 3) == 3
+    
+  test "bsearchMaxIt (imperative form)":
+    var res = bsearchMaxIt(0, 10): 
+      it <= 3
+    check res == 3
 
-test "times":
-  var cnt = 0
-  3.times do: 
-    cnt += 1
-  check cnt == 3
+  test "bsearchMax":
+    let res = bsearchMax(0, 10) do (n:int) -> bool: n*n < 10
+    check res == 3
 
-test "sieve result is same as rabinMiller" : 
-  let arr = sieve 100000
-  for i, b in arr: 
-    let r = rabinMiller i
-    check b == r 
+  test "bsearchMin":
+    let res = bsearchMin('a', 'z', proc (c:char): bool = c >= 'c')
+    check res == 'c'
 
-test "reverse iterator":
-  let a = [1,2,3]
+  test "times":
+    var cnt = 0
+    3.times do: 
+      cnt += 1
+    check cnt == 3
 
-  # copy and in-place reverse 
-  var b = a
-  b.reverse 
+  test "..< 10": 
+    var i = 0
+    for j in ..< 10:
+      assert i == j 
+      i.inc 
+    assert i == 10 
 
-  # iterator should be the same as array
-  var i = 0 
-  for e in a.reverse: 
-    check e == b[i]
-    i.inc 
+  test "forSum":
+    var res = ""
+    var n = 0
+    forSum i in 1..3, "abc":
+      res = res & $i 
+      n.inc 
+    assert res == "123abc"
+    assert n == 6
 
-test "pow":
-  for b in 2..5:
-    var t = 1 
-    for n in 0..10:
-      check pow(b,n) == t
-      t *= b 
+  test "forProd":
+    var res : seq[(int,char)]
+    var n = 0
+    forProd i,j in [1,2], "ab":
+      res.add (i,j) 
+      n.inc
+    assert res == [(1,'a'),(1,'b'),(2,'a'),(2,'b')]
+    assert n == 4
 
-test "pow with modulo":
-  for m in [2,3,5,7,11]:
+  test "forMin":
+    forMin i,j in [1,2,3], [4,5,6]:
+      assert i + 3 == j
+
+    forMin i,j in [1,2,3], "abcdefg":
+      assert i + ord('a') == ord(j) + 1
+
+
+
+suite "prime": 
+  test "sieve result is same as rabinMiller" : 
+    let arr = sieve 100000
+    for i, b in arr: 
+      let r = rabinMiller i
+      check b == r 
+
+suite "array": 
+  test "reverse iterator":
+    let a = [1,2,3]
+
+    # copy and in-place reverse 
+    var b = a
+    b.reverse 
+
+    # iterator should be the same as array
+    var i = 0 
+    for e in a.reverse: 
+      check e == b[i]
+      i.inc 
+
+  test "groupBy":
+    let x = [(1,2),(1,3),(2,4),(3,5)]
+    let y = x.groupBy do (x:auto) -> auto: x[0]
+    assert y[1] == @[(1,2),(1,3)]
+    assert y[2] == @[(2,4)]
+    assert y[3] == @[(3,5)]
+
+
+suite "arithmetic":
+  test "pow":
     for b in 2..5:
       var t = 1 
       for n in 0..10:
-        check pow(b,n,m) == t mod m
-        t *= b mod m 
+        check pow(b,n) == t
+        t *= b 
 
-test "argmin":
-  let a = [2,3,1,4]
-  check argmin(a) == 2 
+  test "pow with modulo":
+    for m in [2,3,5,7,11]:
+      for b in 2..5:
+        var t = 1 
+        for n in 0..10:
+          check pow(b,n,m) == t mod m
+          t *= b mod m 
 
-test "argmin should return -1 if array is empty":
-  let a: seq[int] = @[]
-  check argmin(a) == -1
+  test "argmin":
+    let a = [2,3,1,4]
+    check argmin(a) == 2 
 
-test "fac":
-  check fac(0) == 1
-  check fac(10) == 3628800
+  test "argmin should return -1 if array is empty":
+    let a: seq[int] = @[]
+    check argmin(a) == -1
 
-test "binom":
-  for i,n in [1,2,1]:
-    check binom(2,i) == n
-  for i,n in [1,3,3,1]:
-    check binom(3,i) == n
-  for i,n in [1,10,45,120,210,252,210,120,45,10,1]:
-    check binom(10,i) == n
-  
-test "extgcd":
-  for v in [(3,5,2,-1), (23,101,22,-5)]:
-    let (a,b,m,n) = v
-    var x,y = 0
-    let g = extgcd(a,b,x,y)
-    check g == 1 
-    check x == m 
-    check y == n
+  test "fac":
+    check fac(0) == 1
+    check fac(10) == 3628800
 
-test "inv":
-  for m in [2,3,5,7,101]:
-    for i in 1 ..< m:
-      let t = inv(i,m)
-      check (i*t mod m) == 1
+  test "binom":
+    for i,n in [1,2,1]:
+      check binom(2,i) == n
+    for i,n in [1,3,3,1]:
+      check binom(3,i) == n
+    for i,n in [1,10,45,120,210,252,210,120,45,10,1]:
+      check binom(10,i) == n
+    
+  test "extgcd":
+    for v in [(3,5,2,-1), (23,101,22,-5)]:
+      let (a,b,m,n) = v
+      var x,y = 0
+      let g = extgcd(a,b,x,y)
+      check g == 1 
+      check x == m 
+      check y == n
 
-test "groupBy":
-  let x = [(1,2),(1,3),(2,4),(3,5)]
-  let y = x.groupBy do (x:auto) -> auto: x[0]
-  assert y[1] == @[(1,2),(1,3)]
-  assert y[2] == @[(2,4)]
-  assert y[3] == @[(3,5)]
+  test "inv":
+    for m in [2,3,5,7,101]:
+      for i in 1 ..< m:
+        let t = inv(i,m)
+        check (i*t mod m) == 1
+
+
 
