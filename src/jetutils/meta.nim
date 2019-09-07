d2ffc9dead96b8ce329af8a394059c711a1845b3
Jack Tang <me@jackhftang.com>
2019-09-07 12:25:20 +0800

    update vectorize

--- src/jetutils/meta.nim
+++ src/jetutils/meta.nim
@@ -1,12 +1,26 @@
 import sequtils 
 
-template vectorize*(T,f: untyped): untyped = 
-  ## ..code-block: nim
-  ##  proc double(x:int): int = 2*x
-  ##  check double([1,2,3]) == @[2,4,6]
+template vectorize*(f,T: untyped): untyped = 
+  runnableExamples:
+    proc double(x:int): int = 2*x
+    vectorize(double, int)
+    assert double([1,2,3]) == @[2,4,6]
   type
     S = typeof(f(new(T)[]))
   proc `f`(xs: openArray[T]): seq[S] =
     result = newSeq[S](xs.len)
     for i,x in xs: result[i] = `f`(x)
 
+template vectorize*(f,T,S: untyped): untyped =
+  runnableExamples:
+    vectorize(`+`,int,int)
+    assert [1,2] + [3,4] == @[4,6]
+  type
+    outType = typeof(f(new(T)[], new(S)[]))
+  proc `f`(xs: openArray[T], ys: openArray[S]): seq[outType] =
+    let l = xs.len
+    if l != ys.len: raise newException(ValueError, "Non-equal length")
+    result = newSeq[outType](l)
+    for i in 0 ..< l: result[i] = f(xs[i], ys[i])
+
+  
--- tests/jetutils/meta_test.nim
+++ tests/jetutils/meta_test.nim
@@ -2,10 +2,13 @@ import unittest, ../../src/jetutils
 
 suite "meta":
 
-  test "vectorize":    
+  test "vectorize(f,T)":    
     proc double(x:int): int = 2*x
-    vectorize(int,double)
+    vectorize(double,int)
     check double([1,2,3]) == @[2,4,6]
 
+  test "vectorize(f,T,S)":
+    vectorize(`+`,int,int)
+    check [1,2] + [3,4] == @[4,6]
 
   
