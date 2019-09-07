844789128aba855126dbfeea355f91144b5521c9
Jack Tang <me@jackhftang.com>
2019-09-07 15:21:42 +0800

    improve docs and error message of vectorize

--- src/jetutils/meta.nim
+++ src/jetutils/meta.nim
@@ -1,6 +1,7 @@
 import sequtils 
 
 template vectorize*(f,T: untyped): untyped = 
+  ## make a unary operation applicable over seq[T]
   runnableExamples:
     proc double(x:int): int = 2*x
     vectorize(double, int)
@@ -12,6 +13,7 @@ template vectorize*(f,T: untyped): untyped =
     for i,x in xs: result[i] = `f`(x)
 
 template vectorize*(f,T,S: untyped): untyped =
+  ## make a binary operation applicable over (seq[T], seq[S])
   runnableExamples:
     vectorize(`+`,int,int)
     assert [1,2] + [3,4] == @[4,6]
@@ -19,8 +21,6 @@ template vectorize*(f,T,S: untyped): untyped =
     outType = typeof(f(new(T)[], new(S)[]))
   proc `f`(xs: openArray[T], ys: openArray[S]): seq[outType] =
     let l = xs.len
-    if l != ys.len: raise newException(ValueError, "Non-equal length")
+    if l != ys.len: raise newException(ValueError, "cannot vectorize over unequal length")
     result = newSeq[outType](l)
     for i in 0 ..< l: result[i] = f(xs[i], ys[i])
-
-  
